7aa1ae9ccdf467f3cd782759ee57ffcef9bc4915
maegan <maegan.burke@gmail.com>
2016-08-16 19:21:33 +0700

    updated with blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   get '/' => 'sites#home'
-  get '/marketing' => 'sites#marketing'
+  get '/custom_resume_design' => 'sites#resumes'
+  get '/custom_marketing_materials' => 'sites#marketing'
+  get '/esl_lessons' => 'sites#english'
+  get '/blog' => 'sites#blog'
 
 end
