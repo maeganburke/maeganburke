ea5c287bdee1b522bd84a13581d3645cdc1dcd91
maegan <maegan.burke@gmail.com>
2016-10-27 17:48:27 +0800

    update

--- app/assets/javascripts/sites.js
+++ app/assets/javascripts/sites.js
@@ -3,3 +3,8 @@ $('#myModal').modal();
 $('#myCarousel').carousel({
   interval: 10000
 })
+
+$("#thumbnailImage").click(function() {
+   $(this).attr('width', '400');
+    $(this).attr('height', '300');
+});
