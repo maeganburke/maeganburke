a2b4f74515b553a25642ee6e980b6ec86dfb3074
maegan <maegan.burke@gmail.com>
2017-03-15 13:24:25 +0800

    more mobile

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
   get '/' => 'sites#home'
-  get '/custom_resume_design' => 'sites#resumes'
-  get '/custom_marketing_materials' => 'sites#marketing'
-  get '/tellmae' => 'tellmae#tellmae'
-  get '/blog' => 'sites#blog'
+  get '/hear_more' => 'sites#hear_more'
 
 end
